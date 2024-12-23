174c22ac832be385bd0987d85ae25c7b0d550c56
Nonilium <nonilium@gmail.com>
2024-12-23 15:37:21 +0300

    Update network.py

--- network.py
+++ network.py
@@ -2,20 +2,26 @@ import numpy as np
 from keras import *
 from keras.src.datasets import *
 from keras.src.layers import *
+import ascii
 
+print(ascii.file_ascii)
 
-(x_train, y_train), (x_test, y_test) = open('Ramanyana.txt', 'r')
+path_abs = r'C:\Users\tyuti\IdeaProjects\Gutenberg\Ramanyana.txt'
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# open(path_abs, 'r')
 
 model = Sequential()
 
-model.add(Dense(1, activation='relu'))
+model.add(Dense(units=1, activation='relu'))
 
-model.add(Dense(32, activation='relu', input_dim=(28, 28, 1)))
+model.add(Dense(units=32, activation='relu', input_dim=784))
 
-model.add(Dense(100, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(100, activation='relu'))
+model.add(Dense(units=10, activation='relu'))
 
-model.compile(loss='mse', optimizer='adam')
 
-model.summary()
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10)
