094949f414d33932499c78260188640fe748880a
Nonilium <nonilium@gmail.com>
2024-12-23 15:21:02 +0300

    Update network.py

--- network.py
+++ network.py
@@ -1,9 +1,21 @@
 import numpy as np
-from tensorflow.keras.models import *
-from tensorflow.keras.layers import *
+from keras import *
+from keras.src.datasets import *
+from keras.src.layers import *
 
-from tensorflow.keras.layers import Dense
+
+(x_train, y_train), (x_test, y_test) = open('Ramanyana.txt', 'r')
 
 model = Sequential()
 
-model.add(Dense(1, input_shape=(1,), activation='sigmoid'))
+model.add(Dense(1, activation='relu'))
+
+model.add(Dense(32, activation='relu', input_dim=(28, 28, 1)))
+
+model.add(Dense(100, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(100, activation='relu'))
+
+model.compile(loss='mse', optimizer='adam')
+
+model.summary()
