a059f7116b4a579b100e7059aad64c0a6e81f464
Nonilium <nonilium@gmail.com>
2024-12-24 11:49:36 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,33 +1,28 @@
 # -*- coding: utf-8 -*-
+
 import requests as rq
-# from ascii import file_ascii as file
 from bs4 import BeautifulSoup as bs
 
 
 class Parser:
     def __init__(self):
-        self.agent = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/51.0.2704.103 Safari/537.36")
-        self.url = ("https://www.archive.org/" + str("details/booksbylanguage_indonesian?tab=collection"))
-        self.classes = ["status-text", "hide-text", "cell-container", "block_container", "col"]
+        self.agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36"
+        self.url = "https://www.archive.org"
+        self.classes = ["desktop", "status-text", "hide-text", "cell-container", "block_container", "col"]
         self.id = ["item-size", "icon"]
         self.name = ["div", "span", "h4", "p", "a", "article", "col"]
         self.features = "html.parser"
         self.soup = bs(self.response().text, self.features)
-        self.block_container = self.soup.find(class_=" grid ")
-        self.view = self.soup.find(name="li", class_="icon")
+        self.view = self.soup.findAll(name="div", class_=self.classes[0])
 
     def response(self):
         return rq.get(self.url)
 
-    def check_view(self):
-        return self.soup
+    def text_page(self):
+        return self.response().text
 
-    def title(self):
-        return self.block_container()
-
-    def status(self):
-        return self.view()
+    def soup_page(self):
+        return self.view
 
 
 if __name__ == '__main__':
@@ -35,9 +30,9 @@ if __name__ == '__main__':
     p = Parser()
 
     status = p.response().status_code
-    title = p.title()
+    find = p.soup_page()
 
     if status == 200:
         print("Status Code:", status)
 
-    print(title)
+    print(find)
