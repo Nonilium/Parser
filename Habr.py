2d8bb947503143542f2a9bb7e6156d50dd5fd211
Nonilium <nonilium@gmail.com>
2024-12-26 15:00:59 +0300

    Add files via upload

--- Habr.py
+++ Habr.py
@@ -1,25 +1,33 @@
+import time
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
+start = time.time()
 
-url = "https://habr.com/ru/articles/top/weekly/easy/"
+for i in range(1, 10**5 + 1):
+    url = f"https://habr.com/ru/articles/{i}/"
 
-response = rq.get(url)
+    response = rq.get(url)
 
-soup = bs(response.text, "html.parser")
+    soup = bs(response.text, "html.parser")
 
-title = soup.findAll("a", class_="tm-title__link")
+    title = soup.findAll(name="h1", class_="tm-title tm-title_h1")
 
-mas = []
+    page = soup.findAll(name="span", class_="tm-pagination__page tm-pagination__page_current")
 
-for ti in title:
-    title_words = ti.text
+    mas = []
 
-    with open(r"C:\Eagle\Parser\habr\Data_habr\title.txt", "r+") as f:
-        mas.append(title_words)
+    for ti in title:
+        title_words = ti.text
+        with open(r"C:\Eagle\Parser\habr\Data_habr\title.txt", "w", encoding="utf-8") as f:
+            mas.append(title_words)
 
-        for i in mas:
-            f.write(i)
+            f.write(title_words)
 
-for mas in mas:
-    print(mas)
+            for m in mas:
+                print(m)
+
+
+end = time.time()
+
+print(end - start)
